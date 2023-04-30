a6f590e1f173ff879425c6ac48117dad980f8f1f
Andrii Yelnikov <andriiyelnikov@gmail.com>
2023-05-01 00:14:04 +0300

    change database

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('test_zusn', 'test_zusn_user', 'kJrUAYN3i4Q5D6ZSVrDcePPNrsIAXHhF', {
+export const sequelize = new Sequelize('test1_z267', 'test1_z267_user', 'qrg9dYQEhY2ERzNuigfXKFeBLl9GVu54', {
   // host: 'dpg-cf962jarrk0e2av109fg-a.oregon-postgres.render.com',
-  host: 'dpg-cf962jarrk0e2av109fg-a',
+  host: 'dpg-ch5v5p5gk4qc132r5vhg-a',
   dialectOptions: {
     ssl: {
       require: true,
